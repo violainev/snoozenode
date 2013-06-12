71843e1c5f342daaef2b5ce197892388055ad70d
Violaine Villebonnet <vi.villebonnet@gmail.com>
2013-06-12 14:24:29 +0200

    Separate build and build package

--- distributions/deb-package/prebuild.sh
+++ distributions/deb-package/prebuild.sh
@@ -22,5 +22,13 @@
 SNOOZE_PACKAGE_NAME="snoozenode"
 SNOOZE_JAR_NAME="uber-snoozenode-1.1.0-SNAPSHOT.jar"
 
-cd $SNOOZE_PACKAGE_NAME
-./rules
+# Update config files
+cp -R ../../configs/framework/quartz.properties $SNOOZE_PACKAGE_NAME/debian/input/configs/
+cp -R ../../configs/framework/snooze_node.cfg $SNOOZE_PACKAGE_NAME/debian/input/configs/
+cp -R ../../configs/framework/log4j.xml $SNOOZE_PACKAGE_NAME/debian/input/configs/
+cp -R ../../configs/init_script/snoozenode $SNOOZE_PACKAGE_NAME/debian/input/init.d/
+cp -R ../../configs/powermanagement/pm-utils-hook/00snoozenode \
+                                    $SNOOZE_PACKAGE_NAME/debian/input/pm-utils/
+
+# Update snooze jar
+cp -R ../../target/$SNOOZE_JAR_NAME $SNOOZE_PACKAGE_NAME/debian/input/snoozenode.jar
